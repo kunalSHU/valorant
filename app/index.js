0862058168f065f221e59ad0f132f60e3939cad3
ShabazBadshah <badshah.shabaz@gmail.com>
2020-05-21 18:08:34 -0400

    Basic app with Jest test

---
+++ app/index.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var express_graphql = require("express-graphql");
+var { buildSchema } = require("graphql");
+var cors = require("cors");
+// GraphQL schema
+var schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+// Root resolver
+var root = {
+  message: () => "Hello World!",
+};
+// Create an express server and a GraphQL endpoint
+var app = express();
+app.use(cors());
+app.use(
+  "/graphql",
+  express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+app.listen(8080, () =>
+  console.log("Express GraphQL Server Now Running On localhost:8080/graphql")
+);

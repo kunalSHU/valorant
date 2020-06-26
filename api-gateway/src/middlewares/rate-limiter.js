bde6214c00fdee7c3cd0d2a42159944bfc0dfd40
ShabazBadshah <badshah.shabaz@gmail.com>
2020-06-25 22:55:59 -0400

    Autogenerate source code jsdocs via doxdox

--- api-gateway/src/middlewares/rate-limiter.js
+++ api-gateway/src/middlewares/rate-limiter.js
@@ -6,11 +6,11 @@ const httpStatus = require('../utils/http-status-code.js');
  * Allows a maximum number of requests to the server from a client within a certain timeframe.
  *
  * @example
- * Only allow 10 requests per minute: rateLimiter(60000, 10)
+ * Only allow 10 requests per minute: rateLimiter(60000, 10, onMaxRequestsExceeded)
  *
  * @param {number} maxRequestWindowMs - The length of the rate limiting time window in milliseconds.
  * @param {number} maxRequestsAllowedPerWindow - The maximum amount of requests that can be made within the time window.
- * @param {Function} onMaxRequestsExceeded - Function that will run when.
+ * @param {Function} onMaxRequestsExceeded - The function that will run when the maximum number of requests have been exceeded by the client.
  * @returns {void}
  */
 const rateLimiter = (maxRequestWindowMs = 1000, maxRequestsAllowedPerWindow = 200, onMaxRequestsExceeded) =>
@@ -22,8 +22,8 @@ const rateLimiter = (maxRequestWindowMs = 1000, maxRequestsAllowedPerWindow = 20
     /**
      * Executes when the MAX_REQUESTS_PER_WINDOW is exceeded by calling the onMaxRequestExceeded function provided to rateLimiter object.
      *
-     * @param {Request} req - The express request object that is being recieved from the client.
-     * @param {Response} res - The express response object that will be sent back to the client.
+     * @param {express.Request} req - The express request object that is being recieved from the client.
+     * @param {express.Response} res - The express response object that will be sent back to the client.
      * @param {Function} [next] - Calls the next middleware in the middleware stack.
      * @returns {void}
      */

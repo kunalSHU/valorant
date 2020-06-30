7aec551703409fb0b43096c1d2925eaff07debbd
Swetha Maramganty <swethamaramganty@gmail.com>
2020-06-30 17:44:04 -0400

    Adding db related scripts

---
+++ services/medical-conditions/db/patient_info.sql
@@ -0,0 +1,26 @@
+DROP SCHEMA IF EXISTS PATIENT_INFO CASCADE;
+CREATE SCHEMA PATIENT_INFO;
+SET search_path TO PATIENT_INFO;
+
+CREATE TABLE patient_basic_info_tbl(
+    userid         INTEGER     NOT NULL PRIMARY KEY,
+    addressid      INTEGER     NOT NULL FOREIGN KEY REFERENCES address_info_tbl(addressid),
+    username       VARCHAR     NOT NULL,
+    first_name     VARCHAR     NOT NULL,
+    last_name      VARCHAR     NOT NULL,
+    phone_number   INTEGER     NOT NULL,
+    email          VARCHAR     NOT NULL,
+    birthdate      DATE        NOT NULL,
+    date_became_patient DATE   NOT NULL,
+    gender         TEXT(1)     NOT NULL
+);
+
+CREATE TABLE address_info_tbl(
+    addressid      INTEGER     NOT NULL PRIMARY KEY,
+    streetname     VARCHAR     NOT NULL,
+    city           VARCHAR     NOT NULL,
+    postal_code    VARCHAR     NOT NULL,
+    province       VARCHAR     NOT NULL,
+    country        VARCHAR     NOT NULL,
+    otherdetails   VARCHAR
+);

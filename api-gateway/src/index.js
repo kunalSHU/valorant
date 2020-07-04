811d2e432b1bd7ff1e5e6e6b2f4b050bcdaf0c3e
ShabazBadshah <badshah.shabaz@gmail.com>
2020-07-04 02:36:27 -0400

    Test docker environment complete. DB+app can communicate
    - Introduced .env files for simpler environment configurations and containers
    - The test environment will copy the intial db schema into its container and initilize it. The application container will then be able to connect to the DB.

--- api-gateway/src/index.js
+++ api-gateway/src/index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const bodyParserMiddleware = require('body-parser');
 const cookieParserMiddleware = require('cookie-parser');
 const process = require('process');
+const { Sequelize } = require('sequelize');
 
 const expressIpMiddleware = require('express-ip');
 const corsMiddleware = require('./middlewares/cors-middleware.js');
 const rateLimiterMiddleware = require('./middlewares/rate-limiter-middleware.js');
 
 const environmentConfig = require('./environment-config.json');
+const DEFAULT_GATEWAY_PORT = 8085;
 
-const PORT = process.env.PORT || environmentConfig.application.port;
+const PORT = process.env.APP_PORT || DEFAULT_GATEWAY_PORT;
 const app = express();
 
 app.use(corsMiddleware);
@@ -30,9 +32,33 @@ app.use(
 
 app.use('/', require('./routes/routes.js'));
 
+const POSTGRES_DB_HOST = process.env.POSTGRES_DB_HOST || '127.0.0.1';
+const POSTGRES_DB_NAME = process.env.POSTGRES_DB_NAME || 'accounts_db';
+const POSTGRES_USER = process.env.POSTGRES_USER || 'postgres';
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || 'postgres';
+
+const DB_URI = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_DB_HOST}:5432/${POSTGRES_DB_NAME}`;
+
+const connection = new Sequelize(DB_URI, {
+  dialect: 'postgres',
+  logging: true,
+  operatorsAliases: Sequelize.Op
+});
+
+console.info(`Connecting to database: ${DB_URI}`);
+
+connection
+  .authenticate()
+  .then(() => {
+    console.info(`INFO - Connecting to database: ${DB_URI}`);
+  })
+  .catch((err) => {
+    console.error('ERROR - Unable to connect to the database:', err);
+  });
+
 const server = app
   .listen(PORT, () => {
-    console.log(`API Gateway running on localhost:${PORT}`);
+    console.log(`API Gateway running on localhost:${PORT} in ${process.env.NODE_ENV} mode`);
   })
   .on('error', (error) => {
     console.error('Port in use. Existing program!');

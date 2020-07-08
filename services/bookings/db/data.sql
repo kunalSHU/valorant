4d3c5351ebdd5b6e967e2a8707a8de61b93394b0
Swetha Maramganty <swethamaramganty@gmail.com>
2020-07-08 16:14:27 -0400

    Fixing db files

--- services/bookings/db/data.sql
+++ services/bookings/db/data.sql
@@ -1,16 +1,35 @@
-INSERT INTO bookings_info.address_info_tbl VALUES (
-	    1, 'DisneyLand', 'Idk', 'idk', 'somewhereinamerica', 'whoknows', 'hell'
+
+
+INSERT INTO bookings_info.medication_tbl(
+1, 'Kunaloacid', 100, 'wery effectiwe'
 );
 
-INSERT INTO patient_info.address_info_tbl VALUES (
-	    2, 'WhiteHouses', 'trump', 'donald', 'huuuugee', 'Iknoweverything', 'hell'
+INSERT INTO bookings_info.medication_tbl(
+2, 'Swethaoacid', 10, 'wery good'
 );
 
-INSERT INTO patient_info.patient_basic_info_tbl VALUES (
-    1, 1, 'swethaa_m', 'Swetha', 'Maramganty', 0000000000, 'ahah@gmail.com', '04-12-1997', '06-30-2020', 'F'
+INSERT INTO bookings_info.medication_tbl(
+3, 'Dodooacid', 25, 'wery dodo'
 );
 
-INSERT INTO patient_info.patient_basic_info_tbl VALUES (
-    2, 2, 'kunkun', 'Kunal', 'Shukla', 0000000000, 'kunkun@gmail.com', '04-01-1996', '06-30-2020', 'O'
-);
+INSERT INTO bookings_info.questionaire_tbl( 1, 'yes', 'yes', 'yes',  'yes', 'no', 'no','no','no','yes');
 
+INSERT INTO bookings_info.questionaire_tbl( 2, 'no', 'yes', 'yes',  'yes', 'yes', 'no','yes','no','yes');
+
+INSERT INTO bookings_info.questionaire_tbl( 3, 'no', 'no', 'yes',  'no', 'yes', 'no','yes','no','yes');
+
+
+INSERT INTO bookings_info.appointments_info_basic_tbl(1, 1, 1, 1, '2020-04-12 05:05:25', '2020-04-12 05:05:25', 
+        '2020-04-12 05:05:25', 'In-Person', 'Upcoming', NULL, NULL, NULL);
+
+INSERT INTO bookings_info.appointments_info_basic_tbl(2, 2, 2, 1, '2020-04-12 05:05:25', '2020-04-12 05:05:25', 
+        '2020-04-12 05:05:25', 'In-Person', 'Upcoming', NULL, NULL, NULL);
+
+INSERT INTO bookings_info.appointments_info_basic_tbl(3, 3, 3, 3, '2020-04-12 05:05:25', '2020-04-12 05:05:25', 
+        '2020-04-12 05:05:25', 'In-Person', 'Cancelled', NULL, NULL, NULL);
+    
+INSERT INTO bookings_info.appointments_info_basic_tbl(4, 2, 3, 1, '2020-04-12 05:05:25', '2020-04-12 05:05:25', 
+        '2020-04-12 05:05:25', 'In-Person', 'Completed', 1, 10, '100');
+
+INSERT INTO bookings_info.appointments_info_basic_tbl(5, 4, 3, 1, '2020-04-12 05:05:25', '2020-04-12 05:05:25', 
+        '2020-04-12 05:05:25', 'In-Person', 'Completed', 100, 10, '100');

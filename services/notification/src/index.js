f9f51e971bc494a21398baeddee9f71326c5283d
ShabazBadshah <badshah.shabaz@gmail.com>
2020-07-09 04:48:11 -0400

    Added healthcheck endpoint to graphql server

--- services/notification/src/index.js
+++ services/notification/src/index.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 const express = require('express');
 const express_graphql = require('express-graphql');
 const buildSchema = require('graphql').buildSchema;
 const cors = require('cors');
-const { Pool, Client } = require('pg');
+const { Client } = require('pg');
 
 const PORT = process.env.APP_PORT || 8085;
 // GraphQL schema
@@ -13,14 +12,9 @@ type Query {
     }
 `);
 
-/** NOTE: Change to bookings-db once created.. Change port as well*/
-const client = new Client({
-  user: 'postgres',
-  password: 'postgres',
-  host: '198.91.129.107',
-  port: 8080,
-  database: 'medical-conditions-db'
-});
+const credentials = require('../client-model');
+credentials.database = 'notification-db';
+const client = new Client(credentials);
 
 // Will query the tables here
 const queryFunction = function () {
@@ -68,64 +62,3 @@ const server = app
   });
 
 module.exports = server;
-=======
-const express = require('express');
-const express_graphql = require('express-graphql');
-const buildSchema = require('graphql').buildSchema;
-const cors = require('cors');
-const {Client} = require('pg');
-
-const PORT = process.env.APP_PORT || 8085;
-// GraphQL schema
-const schema = buildSchema(`
-type Query {
-        message: String
-    }
-`);
-
-const credentials = require('../client-model')
-credentials.database = 'notification-db';
-const client = new Client(credentials)
-
-// Will query the tables here
-const queryFunction = function () {
-  client.connect()
-  .then(() => {console.log('Connected Successfully')})
-  .then(() => client.query('SELECT * FROM people'))
-  .then((result) => {console.table(result.rows)})
-  .catch(e => console.log(e))
-  .finally(() => client.end())
-}
-  
-// timeout used so connection to db happens after it is started
-setTimeout(queryFunction , 5000)
-
-// Root resolver
-const root = {
-  message: () => 'Hello World!'
-};
-
-// Create an express server and a GraphQL endpoint
-const app = express();
-app.use(cors());
-app.use(
-  '/graphql',
-  express_graphql({
-    schema: schema,
-    rootValue: root,
-    graphiql: true
-  })
-);
-
-const server = app
-  .listen(PORT, () => {
-    console.log(`Express GraphQL Server Now Running On localhost:${PORT}/graphql`);
-  })
-  .on('error', (error) => {
-    console.log('Port in use. Existing program');
-    console.error(error);
-    process.exit(1);
-  });
-
-module.exports = server;
->>>>>>> master

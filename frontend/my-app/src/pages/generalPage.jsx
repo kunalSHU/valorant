f065357acd0f0c39cda56af8efd0daee07b5b318
kunalSHU <kunalshukla@hotmail.com>
2020-07-17 16:19:07 -0400

    Some modifications using apollo with react

--- frontend/my-app/src/pages/generalPage.jsx
+++ frontend/my-app/src/pages/generalPage.jsx
@@ -9,25 +9,43 @@ import {
     FormInput
   } from "shards-react";
 
-  const PATIENT_RECORD_QUERY = gql`
-    query {
-      message
-    }
-  `;
+const PATIENT_RECORD_QUERY = gql`
+  query {
+    message
+  }
+`;
 
+//Trying to retrieve data from patient record db and displaying it in UI
+const client  = new ApolloClient({
+  uri: 'http://localhost:8087/graphql'
+})
 
-  //Trying to retrieve data from patient record db and displaying it in UI
-  const client  = new ApolloClient({
-    uri: 'http://localhost:8087/graphql'
-  })
+class GeneralPage extends React.Component{
 
-  const queryResult =  () => {
-    console.log("In hEREs")
+  state = {queryResult: false}
+
+  queryResult =  () => {
+    return(
+      <div>      
+        <ApolloProvider client={client}>
+          
+          <Query query={PATIENT_RECORD_QUERY}>
+          {
+            ({loading, error, data}) => {
+              if(loading) return <h4>Loading...</h4>
+              if(error) console.log(error)
+              return <h1>{data.message}</h1>
+            }
+          }
+          </Query>
+        </ApolloProvider>
+      </div>
+
+    )
   }
 
-  const GeneralPage = () => {
+  render(){
     return (
-      <ApolloProvider client={client}>
         <div>
             <SideNavigation />
             <div style={{padding: "0px 0px 0px 75px"}}>
@@ -87,20 +105,10 @@ import {
                 <label style={{color: "#905EAF", fontSize: "20px", padding: "0px 0px 0px 0px"}}><strong>Family Doctor:</strong></label>
                 <label id="familyDoctor" style={{color: "#905EAF", fontSize: "20px", padding: "0px 0px 0px 10px"}}>Dr. wasteman</label>
             </div>
-            <button onClick={queryResult}>Get Result from query in console</button>
-            <Query query={PATIENT_RECORD_QUERY}>
-            {
-              ({loading, error, data}) => {
-                if(loading) return <h4>Loading...</h4>
-                if(error) console.log(error)
-                console.log(data)
-                return <h1>test</h1>
-              }
-            }
-          </Query>
+            <button onClick={() => this.setState({queryResult: true})}>Get Result from query in console</button>
+            {this.state.queryResult ? this.queryResult() : null}
         </div>
-      </ApolloProvider>
     );
   }
-  
-  export default GeneralPage;
+}
+export default GeneralPage;

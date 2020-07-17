46ada59bdd5e33adf3ff39132f59ac9d6bdaf121
kunalSHU <kunalshukla@hotmail.com>
2020-07-17 17:31:10 -0400

    Graphql Schema test

--- services/patient-record/src/index.js
+++ services/patient-record/src/index.js
@@ -24,11 +24,7 @@ const schema = require('./schema');
 //     .finally(() => client.end());
 // };
 const PORT = process.env.APP_PORT || 8087;
-// GraphQL schema
-const schema = buildSchema(`
-type Query {
-  message: String}
-`);
+const schema = require('./schema');
 
 // // timeout used so connection to db happens after it is started
 // setTimeout(queryFunction, 5000)
--- services/patient-record/src/schema.js
+++ services/patient-record/src/schema.js
@@ -1,30 +1,8 @@
 const buildSchema = require('graphql').buildSchema;
 
 const schema = buildSchema(`
-  type Query {
-    message: String
-    userAddress: [UserAddress]
-  }
-  type Mutation {
-    post(
-    addressid: String, 
-    streetname: String
-    city: String
-    postalCode: String
-    province: String
-    country: String
-    otherdetails: String
-    ): UserAddress!
-  }
-  type UserAddress {
-    addressid: String
-    streetname: String
-    city: String
-    postalCode: String
-    province: String
-    country: String
-    otherdetails: String
-  }
+type Query {
+  message: String}
 `);
 
 module.exports = schema;

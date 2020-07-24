acac733d58f0c8f43cf6f73bf89bdc16ec847ed8
kunalSHU <kunalshukla@hotmail.com>
2020-07-24 11:26:14 -0400

    Added insert query for register

--- services/accounts/src/index.js
+++ services/accounts/src/index.js
@@ -5,15 +5,7 @@ const cors = require('cors');
 
 const PORT = 8081;
 // GraphQL schema
-<<<<<<< HEAD
 const schema = require('./schema');
-=======
-const schema = buildSchema(`
-type Query {
-        message: String
-    }
-`);
->>>>>>> 42f4168... Modified accounts
 // const credentials = require('../client-model');
 // credentials.database = 'accounts_db';
 // const client = new Client(credentials);
@@ -32,64 +24,69 @@ type Query {
 //     .catch((e) => console.log(e))
 //     .finally(() => client.end());
 // };
-<<<<<<< HEAD
 
 // // timeout used so connection to db happens after it is started
 // setTimeout(queryFunction, 5000);
 
-const Knex = require("knex");
+const Knex = require('knex');
 const knex = Knex({
   client: 'pg',
-  connection: { 
-    host: '142.1.46.70', 
-    user: 'postgres', 
-    password: 'postgres', 
-    database: 'patient_db', 
+  connection: {
+    host: '142.1.46.70',
+    user: 'postgres',
+    password: 'postgres',
+    database: 'patient_db',
     port: 8088
-    },
-
-=======
-
-// // timeout used so connection to db happens after it is started
-// setTimeout(queryFunction, 5000);
-
-const Knex = require("knex");
-const knex = Knex({
-  client: 'pg',
-  connection: { 
-    host: '142.1.46.70', 
-    user: 'postgres', 
-    password: 'postgres', 
-    database: 'patient_db', 
-    port: 8088
-    },
-
->>>>>>> 42f4168... Modified accounts
+  }
 });
 
 // Root resolver
 const root = {
-<<<<<<< HEAD
   message: () => 'Hello this is accounts service connecting to patient record db!',
-  addUserInfo: ({userid, addressid, username, first_name, last_name, phone_number, email, birthdate,
-    date_became_patient, gender}) => {
-      return knex("patient_info.patient_basic_info_tbl").insert({
-        userid: userid,
-        addressid: addressid,
-        username: username,
-        first_name: first_name,
-        last_name: last_name,
-        phone_number: phone_number,
-        email: email,
-        birthdate: birthdate,
-        date_became_patient: date_became_patient,
-        gender: gender
-      }).then((data) => console.log(data))
-        .catch((err) => console.log(err))
-    }
-=======
-  message: () => 'Hello this is accounts service connecting to patient record db!'
->>>>>>> 42f4168... Modified accounts
+  addUserInfo: ({
+    userid,
+    addressid,
+    username,
+    first_name,
+    last_name,
+    phone_number,
+    email,
+    birthdate,
+    date_became_patient,
+    gender
+  }) => {
+    return knex('patient_info.patient_basic_info_tbl').insert({
+      message: () => 'Hello this is accounts service connecting to patient record db!',
+      addUserInfo: ({
+        userid,
+        addressid,
+        username,
+        first_name,
+        last_name,
+        phone_number,
+        email,
+        birthdate,
+        date_became_patient,
+        gender
+      }) => {
+        return knex('patient_info.address_info_tbl')
+          .insert({
+            userid: userid,
+            addressid: addressid,
+            username: username,
+            first_name: first_name,
+            last_name: last_name,
+            phone_number: phone_number,
+            email: email,
+            birthdate: birthdate,
+            date_became_patient: date_became_patient,
+            gender: gender
+          })
+          .then((data) => console.log(data))
+          .catch((err) => console.log(err));
+      }
+    });
+  }
 };
 
 // Create an express server and a GraphQL endpoint

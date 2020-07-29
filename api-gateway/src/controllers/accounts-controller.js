7d8b36ba14ad05603975bff1ded0f1468e90c54c
ShabazBadshah <badshah.shabaz@gmail.com>
2020-07-29 05:56:55 -0400

    Find single Account by email

--- api-gateway/src/controllers/accounts-controller.js
+++ api-gateway/src/controllers/accounts-controller.js
@@ -21,4 +21,25 @@ const findAllAccounts = async () => {
   }
 };
 
-module.exports = { findAllAccounts };
+/**
+ * Returns the single user who is associated with the specified email.
+ *
+ * @param {string} email - The email of the Account to be found.
+ *
+ * @returns {object} An object containing the User who matches the email.
+ */
+const findAccountByEmail = async (email) => {
+  const findSingleAccountByEmailQuery = `SELECT * FROM ${accountsTable} WHERE email_address=$1`;
+  const findSingleAccountByEmailQueryVariables = [email];
+
+  try {
+    const { rows } = await db.query(findSingleAccountByEmailQuery, findSingleAccountByEmailQueryVariables);
+    return rows.length === 0 ? {} : rows[0];
+  } catch (err) {
+    throw new Error(
+      `Could not execute query '${findSingleAccountByEmailQuery} with variables '${findSingleAccountByEmailQueryVariables}'. Message: ${err.message}`
+    );
+  }
+};
+
+module.exports = { findAllAccounts, findAccountByEmail };
--- api-gateway/src/routes/users/user-route.js
+++ api-gateway/src/routes/users/user-route.js
@@ -4,10 +4,21 @@ const router = require('express').Router();
 const accountsController = require('../../controllers/accounts-controller.js');
 const httpStatusCode = require('../../utils/http-status-code.js');
 
-router.get('/', async (req, res) => {
+router.get('/all', async (req, res) => {
   try {
     const allUsers = await accountsController.findAllAccounts();
-    res.status(httpStatusCode.OK).json({ users: allUsers });
+    res.status(httpStatusCode.OK).json({ data: allUsers });
+  } catch (err) {
+    res.status(httpStatusCode.SERVER_INTERNAL_ERROR).json(err.message);
+  }
+});
+
+router.get('/findWithEmail', async (req, res) => {
+  const { email } = req.query;
+
+  try {
+    const singleUser = await accountsController.findAccountByEmail(email);
+    res.status(httpStatusCode.OK).json({ data: singleUser });
   } catch (err) {
     res.status(httpStatusCode.SERVER_INTERNAL_ERROR).json(err.message);
   }

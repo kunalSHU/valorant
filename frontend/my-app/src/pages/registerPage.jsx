76f13c5910cd8d63800b9595aa3bf73f84a56a7b
kunalSHU <kunalshukla@hotmail.com>
2020-08-01 14:26:22 -0400

    Added walidation

--- frontend/my-app/src/pages/registerPage.jsx
+++ frontend/my-app/src/pages/registerPage.jsx
@@ -31,6 +31,7 @@ const options = [
     'Male', 'Female'
   ];
 const defaultOption = options[0];
+const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const ADDING_PATIENT = gql`
 mutation addUserInfo($userid: Int, $addressid: Int,$username: String, $first_name: String, $last_name: String,
@@ -164,7 +165,13 @@ class RegisterPage extends React.Component{
                 errors: errors
             })
         }
-        else{
+        if (!re.test(String(email).toLowerCase())){
+            errors["email"] = "Invalid Email Format";
+            this.setState({
+                errors: errors 
+            })
+        }
+        else {
             errors["email"] = "";
             this.setState({
                 errors: errors 
@@ -210,21 +217,83 @@ class RegisterPage extends React.Component{
         this.setState({
             password: event.target.value
         })
-
+        let myString="secret1"
+        console.log(/\d/.test(myString))
         console.log(this.state.formStep1)
         if (this.state.formStep1) {
             console.log(reenterPassword)
             console.log(password)
+            console.log(password == "")
+            if (password == "") {
+                errors["passwordMismatch"] = "Passwords field is empty";
+                this.setState({
+                    errors: errors,
+                    passwordMismatch: true
+                })
+                this.passwordMismatch();
+                return;
+            }
+            if (password.length < 6 || password.length > 128) {
+                errors["passwordMismatch"] = "Passwords length between 6 and 128 characters";
+                this.setState({
+                    errors: errors,
+                    passwordMismatch: true
+                })
+                console.log("length is less than 6")
+                console.log(errors["passwordMismatch"])
+                this.passwordMismatch();
+                return;
+            }
+            if (!/\d/.test(password)) {
+                errors["passwordMismatch"] = "Password must contain at least 1 digit";
+                this.setState({
+                    errors: errors,
+                    passwordMismatch: true
+                })
+                this.passwordMismatch();
+                return;
+            }
+            if (!/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(password)) {
+                errors["passwordMismatch"] = "Password must contain at least 1 special character";
+                this.setState({
+                    errors: errors,
+                    passwordMismatch: true
+                })
+                this.passwordMismatch();
+                return;
+            }
+            if (!/[a-z]/g.test(password)) {
+                errors["passwordMismatch"] = "Password must contain at least 1 lowercase character";
+                this.setState({
+                    errors: errors,
+                    passwordMismatch: true
+                })
+                this.passwordMismatch();
+                return;
+            }
+            if (!/[A-Z]/g.test(password)) {
+                errors["passwordMismatch"] = "Password must contain at least 1 uppercase character";
+                this.setState({
+                    errors: errors,
+                    passwordMismatch: true
+                })
+                this.passwordMismatch();
+                return;
+            }
             if (reenterPassword != password) {
                 console.log("passwords mismatch")
                 errors["passwordMismatch"] = "Passwords dont match";
+                this.setState({
+                    errors: errors,
+                    passwordMismatch: true
+                })
+                this.passwordMismatch();
+                return;
             }
-            else {
-                errors["passwordMismatch"] = "";
-            }
-            this.setState({
-                errors: errors
-            })
+            // else {
+            //     errors["passwordMismatch"] = "";
+            // }
+
         }
     }
 
@@ -409,8 +478,8 @@ class RegisterPage extends React.Component{
         return(
             <div>
             <br></br>
-            <Grid container spacing={1} alignItems="flex-end" style={{position: "relative", left: "5.5%", display: "flex"}}>
-                <Grid item>
+            <Grid container spacing={1} alignItems="flex-start" direction="column" style={{position: "relative", left: window.innerWidth/6.3}}>
+                {/* <Grid item>
                     <TextField id="outlined-basic" label="FirstName" variant="outlined" 
                     onChange={this.firstName} helperText={errors["firstName"]} defaultValue={this.state.firstName}/>
                 </Grid>
@@ -421,12 +490,13 @@ class RegisterPage extends React.Component{
                 <Grid item>
                     <TextField id="outlined-basic" label="PhoneNumber" variant="outlined" 
                     onChange={this.phoneNumber} helperText={errors["phoneNumber"]} defaultValue={this.state.phoneNumber}/>
-                </Grid>
+                </Grid> */}
                 <Grid item>
                     <TextField id="outlined-basic" label="Email" variant="outlined" 
-                    onChange={this.email} helperText={errors["email"]} defaultValue={this.state.email}/>
+                    onChange={this.email} helperText={errors["email"]} defaultValue={this.state.email}
+                    style={{width: window.innerWidth/6.1}}/>
                 </Grid>
-                <Grid item style={{width:"29%"}}>
+                {/* <Grid item style={{width:window.innerWidth/7.1}}>
                     <TextField
                         id="outlined-select-options"
                         select
@@ -434,10 +504,10 @@ class RegisterPage extends React.Component{
                         value={this.state.gender}
                         onChange={this.gender}
                         variant="outlined"
-                        style={{width: "100%"}}
+                        style={{width: window.innerWidth/7.42}}
                         >
                         {options.map((option) => (
-                            <MenuItem key={option} value={option} style={{width: "105%"}}>
+                            <MenuItem key={option} value={option} style={{width: window.innerWidth/7.42}}>
                             {option}
                             </MenuItem>
                         ))}
@@ -446,7 +516,7 @@ class RegisterPage extends React.Component{
                 <Grid item>
                     <TextField id="outlined-basic" label="UserName" variant="outlined" 
                     onChange={this.userName} helperText={errors["userName"]} defaultValue={this.state.userName}/>
-                </Grid>
+                </Grid> */}
                 <Grid item>
                     <FormControl variant="outlined">
                         <InputLabel htmlFor="outlined-adornment-password" >Password</InputLabel>
@@ -497,10 +567,10 @@ class RegisterPage extends React.Component{
                             />
                     </FormControl>
                 </Grid>
-                <Grid item>
+                {/* <Grid item>
                     <MaterialUIPickers selectedDate={this.state.birthdate}
                     onChange={this.handleDateChange}></MaterialUIPickers>
-                </Grid> 
+                </Grid>  */}
             </Grid>
             <Button variant="contained" color="primary" 
             style={{position: "absolute", left: "45%", top: "87%"}} onClick={this.handleValidation}>
@@ -519,79 +589,80 @@ class RegisterPage extends React.Component{
         })
     }
 
-    stage2Form = () => {
-        return(
-            <div>
-            <br></br>
-            <Grid container spacing={1} alignItems="flex-end" style={{position: "relative", left: "5.5%"}}>
-                <Grid item>
-                    <TextField id="outlined-basic" label="StreetName" variant="outlined" 
-                    onChange={this.streetName} helperText={this.state.errors["streetName"]} defaultValue={this.state.streetName}/>
-                </Grid>
-                <Grid item>
-                    <TextField id="outlined-basic" label="City" variant="outlined" 
-                    onChange={this.city} helperText={this.state.errors["city"]} defaultValue={this.state.city}/>
-                </Grid>
-                <Grid item>
-                    <TextField id="outlined-basic" label="PostalCode" variant="outlined" 
-                    onChange={this.postalCode} helperText={this.state.errors["postalCode"]} defaultValue={this.state.postal_code}/>
-                </Grid>
-                <Grid item>
-                    <TextField id="outlined-basic" label="Province" variant="outlined" 
-                    onChange={this.province} helperText={this.state.errors["province"]} defaultValue={this.state.province}/>
-                </Grid>
-                <Grid item>
-                    <TextField id="outlined-basic" label="Country" variant="outlined"
-                    onChange={this.country} helperText={this.state.errors["country"]} defaultValue={this.state.country}/>
-                </Grid>
-                <Grid item>
-                    <TextField id="outlined-basic" label="OtherDetails" variant="outlined" 
-                    onChange={this.otherDetails} helperText={this.state.errors["otherDetails"]} defaultValue={this.state.otherdetails}/>
-                </Grid>
-            </Grid>
-            <Button color="primary" style={{position: "absolute", 
-            left: "40%", top: "70%"}} variant= "outlined" onClick={this.back}>Back</Button>
+    // stage2Form = () => {
+    //     return(
+    //         <div>
+    //         <br></br>
+    //         <Grid container spacing={1} alignItems="flex-end" style={{position: "relative", left: "5.5%"}}>
+    //             <Grid item>
+    //                 <TextField id="outlined-basic" label="StreetName" variant="outlined" 
+    //                 onChange={this.streetName} helperText={this.state.errors["streetName"]} defaultValue={this.state.streetName}/>
+    //             </Grid>
+    //             <Grid item>
+    //                 <TextField id="outlined-basic" label="City" variant="outlined" 
+    //                 onChange={this.city} helperText={this.state.errors["city"]} defaultValue={this.state.city}/>
+    //             </Grid>
+    //             <Grid item>
+    //                 <TextField id="outlined-basic" label="PostalCode" variant="outlined" 
+    //                 onChange={this.postalCode} helperText={this.state.errors["postalCode"]} defaultValue={this.state.postal_code}/>
+    //             </Grid>
+    //             <Grid item>
+    //                 <TextField id="outlined-basic" label="Province" variant="outlined" 
+    //                 onChange={this.province} helperText={this.state.errors["province"]} defaultValue={this.state.province}/>
+    //             </Grid>
+    //             <Grid item>
+    //                 <TextField id="outlined-basic" label="Country" variant="outlined"
+    //                 onChange={this.country} helperText={this.state.errors["country"]} defaultValue={this.state.country}/>
+    //             </Grid>
+    //             <Grid item>
+    //                 <TextField id="outlined-basic" label="OtherDetails" variant="outlined" 
+    //                 onChange={this.otherDetails} helperText={this.state.errors["otherDetails"]} defaultValue={this.state.otherdetails}/>
+    //             </Grid>
+    //         </Grid>
+    //         <Button color="primary" style={{position: "absolute", 
+    //         left: "40%", top: "70%"}} variant= "outlined" onClick={this.back}>Back</Button>
 
-            <ApolloProvider client={client} useMutation={ADDING_PATIENT}>
-                <Mutation mutation={ADDING_PATIENT}>  
-                    {(adding_patient, { data }) => (
-                        <Button id="submit" value="Submit" variant="contained" color="primary" 
-                        style={{position: "absolute", left: "50%", top: "70%"}} onClick={(e) => {
-                            e.preventDefault();
+    //         <ApolloProvider client={client} useMutation={ADDING_PATIENT}>
+    //             <Mutation mutation={ADDING_PATIENT}>  
+    //                 {(adding_patient, { data }) => (
+    //                     <Button id="submit" value="Submit" variant="contained" color="primary" 
+    //                     style={{position: "absolute", left: "50%", top: "70%"}} onClick={(e) => {
+    //                         e.preventDefault();
 
-                            //check to see if form is valid first
-                            //otherwise we make post requests
-                            if (!this.state.isFormValid) {
-                                this.handleValidation();
-                            }
-                            else { 
-                                adding_patient({
-                                    variables: {
-                                        userid: this.state.userid, addressid: this.state.addressid,
-                                        username: this.state.userName, first_name: this.state.firstName,
-                                        last_name: this.state.lastName, phone_number: this.state.phoneNumber,
-                                        email: this.state.email, birthdate: this.state.birthdate,
-                                        date_became_patient: this.state.dateRegistered,
-                                        gender: this.state.gender
-                                    }
-                                })
-                            }
-                            console.log(this.state.phoneNumber)
-                            console.log(typeof(this.state.phone))
-                        }}>
-                            Submit 
-                        </Button>
-                    )}
-                </Mutation>
-            </ApolloProvider>
-        </div>
-        )
-    }
+    //                         //check to see if form is valid first
+    //                         //otherwise we make post requests
+    //                         if (!this.state.isFormValid) {
+    //                             this.handleValidation();
+    //                         }
+    //                         else { 
+    //                             adding_patient({
+    //                                 variables: {
+    //                                     userid: this.state.userid, addressid: this.state.addressid,
+    //                                     username: this.state.userName, first_name: this.state.firstName,
+    //                                     last_name: this.state.lastName, phone_number: this.state.phoneNumber,
+    //                                     email: this.state.email, birthdate: this.state.birthdate,
+    //                                     date_became_patient: this.state.dateRegistered,
+    //                                     gender: this.state.gender
+    //                                 }
+    //                             })
+    //                         }
+    //                         console.log(this.state.phoneNumber)
+    //                         console.log(typeof(this.state.phone))
+    //                     }}>
+    //                         Submit 
+    //                     </Button>
+    //                 )}
+    //             </Mutation>
+    //         </ApolloProvider>
+    //     </div>
+    //     )
+    // }
 
     passwordMismatch = () => {
         console.log("in passowrd mismatch function")
+        console.log(errors["passwordMismatch"])
         return (
-            <div>Passwords do not match or are empty</div>
+            <div style={{position: "relative", left: window.innerWidth/7}}>{errors["passwordMismatch"]}</div>
         )
     }
 
@@ -706,11 +777,11 @@ class RegisterPage extends React.Component{
         //we good for next stage 
         if(errors["firstName"] == "" && errors["lastName"] == "" &&
         errors["phoneNumber"] == "" && errors["email"] == "" && errors["userName"] == ""){
-            this.setState({
-                formStep1: false,
-                formStep2: true,
-                progress: 50
-            })
+            // this.setState({
+            //     formStep1: false,
+            //     formStep2: true,
+            //     progress: 50
+            // })
             
         }
 
@@ -733,9 +804,9 @@ class RegisterPage extends React.Component{
                 <Card variant="outlined" style={{display: 'inline-block', height: window.innerHeight/2, left: window.innerHeight/2.3
                 ,position: "absolute", width: window.innerWidth/2, top: window.innerHeight/4}}>
                     <CardContent>
-                        <ProgressBar animated now={this.state.progress} label={`${this.state.progress}%`}/>   
+                        {/* <ProgressBar animated now={this.state.progress} label={`${this.state.progress}%`}/>    */}
                         {this.state.formStep1 ? this.stage1Form() : null}
-                        {this.state.formStep2 ? this.stage2Form() : null}
+                        {/* {this.state.formStep2 ? this.stage2Form() : null} */}
                         {this.state.passwordMismatch && this.state.formStep1
                         ? this.passwordMismatch() : null}
                     </CardContent>

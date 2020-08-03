77b5d600d9cd2caf30142d66e6819eff68765fe0
ShabazBadshah <badshah.shabaz@gmail.com>
2020-08-03 18:46:26 -0400

    Added templated frontend

---
+++ template-frontend/src/layouts/Dashboard/components/Sidebar/index.jsx
@@ -0,0 +1,161 @@
+import React, { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+// Externals
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+// Material helpers
+import { withStyles } from '@material-ui/core';
+
+// Material components
+import {
+  Avatar,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+  Typography
+} from '@material-ui/core';
+
+// Material icons
+import {
+  InfoOutlined as InfoIcon,
+  SettingsOutlined as SettingsIcon,
+  EventAvailableOutlined as EventAvailableIcon,
+  AssignmentIndOutlined as MedicalRecordIcon,
+  FaceOutlined as AccountIcon
+} from '@material-ui/icons';
+
+// Component styles
+import styles from './styles';
+
+class Sidebar extends Component {
+  render() {
+    const { classes, className } = this.props;
+
+    const rootClassName = classNames(classes.root, className);
+
+    return (
+      <nav className={rootClassName}>
+        <div className={classes.logoWrapper}>
+          <Link
+            className={classes.logoLink}
+            to="/"
+          >
+            <img
+              alt="Valorant logo"
+              className={classes.logoImage}
+              src="/images/logos/logo.png" 
+            />
+          </Link>
+        </div>
+
+        <Divider className={classes.logoDivider} />
+        
+        <List
+          component="div"
+          disablePadding
+        >
+          <ListItem
+            activeClassName={classes.activeListItem}
+            className={classes.listItem}
+            component={NavLink}
+            to="/dashboard"
+          >
+            <ListItemIcon className={classes.listItemIcon}>
+              <EventAvailableIcon />
+            </ListItemIcon>
+            <ListItemText
+              classes={{ primary: classes.listItemText }}
+              primary="Appointments"
+            />
+          </ListItem>
+
+          <ListItem
+            activeClassName={classes.activeListItem}
+            className={classes.listItem}
+            component={NavLink}
+            to="/account"
+          >
+            <ListItemIcon className={classes.listItemIcon}>
+              <AccountIcon />
+            </ListItemIcon>
+            <ListItemText
+              classes={{ primary: classes.listItemText }}
+              primary="Account"
+            />
+          </ListItem>
+          
+          <ListItem
+            activeClassName={classes.activeListItem}
+            className={classes.listItem}
+            component={NavLink}
+            to="/gg"
+          >
+            <ListItemIcon className={classes.listItemIcon}>
+              <MedicalRecordIcon />
+            </ListItemIcon>
+            <ListItemText
+              classes={{ primary: classes.listItemText }}
+              primary="Medical Record"
+            />
+          </ListItem>
+          
+          <ListItem
+            activeClassName={classes.activeListItem}
+            className={classes.listItem}
+            component={NavLink}
+            to="/settings"
+          >
+            <ListItemIcon className={classes.listItemIcon}>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText
+              classes={{ primary: classes.listItemText }}
+              primary="Settings"
+            />
+          </ListItem>
+        </List>
+
+        <Divider className={classes.listDivider} />
+
+        <List
+          component="div"
+          disablePadding
+          subheader={
+            <ListSubheader className={classes.listSubheader}>
+              Support
+            </ListSubheader>
+          }
+        >
+          <ListItem
+            className={classes.listItem}
+            component="a"
+            href="https://devias.io/contact-us"
+            target="_blank"
+          >
+            <ListItemIcon className={classes.listItemIcon}>
+              <InfoIcon />
+            </ListItemIcon>
+            <ListItemText
+              classes={{ primary: classes.listItemText }}
+              primary="Contact Us"
+            />
+          </ListItem>
+        </List>
+      </nav>
+    );
+  }
+}
+
+Sidebar.propTypes = {
+  className: PropTypes.string,
+  classes: PropTypes.object.isRequired
+};
+
+let newStyles;
+[Sidebar, newStyles] = require('../../../../common/customizers').customizers.customizeComponent('Sidebar', Sidebar, styles);
+export default withStyles(newStyles)(Sidebar);
---
+++ template-frontend/src/views/ProductList/components/ProductsToolbar/styles.jsx
@@ -0,0 +1,15 @@
+export default theme => ({
+  root: {},
+  row: {
+    height: '42px',
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: theme.spacing(1)
+  },
+  spacer: {
+    flexGrow: 1
+  },
+  searchInput: {
+    marginRight: theme.spacing(1)
+  }
+});

6dd7c9d3fb08c41fb68ca8bdc058ae0b43373661
ShabazBadshah <badshah.shabaz@gmail.com>
2020-08-06 18:18:23 -0400

    Added vitals info to medical record

--- template-frontend/src/views/MedicalRecord/components/index.jsx
+++ template-frontend/src/views/MedicalRecord/components/index.jsx
@@ -1,3 +1,6 @@
 export { default as NameGender } from './NameGender';
 export { default as Location } from './Location';
 export { default as ConditionsList } from './ConditionsList';
+export { default as WeightTracker } from './WeightTracker';
+export { default as HeightTracker } from './HeightTracker';
+export { default as BloodType } from './BloodType';

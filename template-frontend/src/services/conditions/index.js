7c4747ef126425e9ec4c088f39a9b0c40b532bbf
ShabazBadshah <badshah.shabaz@gmail.com>
2020-08-07 08:06:24 -0400

    Added signatures to retrieve data from BE for record and details

--- template-frontend/src/services/conditions/index.js
+++ template-frontend/src/services/conditions/index.js
@@ -1,42 +1,13 @@
 // Mock data
-import users from "../../data/users";
 import conditions from "../../data/conditions";
 
-export const getAllUsers = () => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve({
-        users: users
-      });
-    }, 700);
-  });
-};
-
-export const updateBasicInfo = () => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve({
-        users: users
-      });
-    }, 700);
-  });
-};
-
-export const updateLocationInfo = () => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve({
-        users: users
-      });
-    }, 700);
-  });
-};
-
 export const getAllConditionsByAccountId = accountId => {
-  // Make request to backend (see conditions file in /data for expected output)
+  // TODO retrieve from server instead of local file
   return new Promise(resolve => {
+    // Make request to backend (see conditions file in /data for expected output)
     setTimeout(() => {
       resolve({
+        // Change
         conditions
       });
     }, 700);

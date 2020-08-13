ea55abcd406f1a9b417d27760c1be2f88e9c5c0d
kunalSHU <kunalshukla@hotmail.com>
2020-08-13 19:27:22 -0400

    User address post

--- frontend/src/services/record/index.js
+++ frontend/src/services/record/index.js
@@ -1,6 +1,9 @@
 // Mock data
 import users from "../../data/users";
 import conditions from "../../data/conditions";
+import axios from 'axios';
+
+const API_GATEWAY = 'http://142.1.46.70:8082';
 
 export const getAllConditionsByAccountId = accountId => {
   // TODO retrieve from server instead of local file
@@ -24,6 +27,23 @@ export const getAllUsers = () => {
     }, 700);
   });
 };
+
+export const postUserAddress = (street, postalCode, city, province) => {
+  console.log(street)
+  console.log(postalCode)
+  console.log(city)
+  console.log(province)
+  axios.post(`${API_GATEWAY}/services/patient-record`, {
+    query: `mutation {
+      postUserAddress(streetname:"${street}" 
+      city:"${city}" postal_code:"${postalCode}" province:"${province}") {
+        addressid
+      }
+    }`
+  }).then((response) => {
+    console.log(response.status)
+  })
+}
 
 export const retrieveBasicInfoByAccountId = accountId => {
   // Expect { firstName, lastName, sex } to be returned from BE

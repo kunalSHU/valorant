27c856282ebdf4de82f74481113c1f401fa3037c
kunalSHU <kunalshukla@hotmail.com>
2020-08-13 11:59:18 -0400

    Fixing addressid query knex

--- services/patient-record/src/index.js
+++ services/patient-record/src/index.js
@@ -45,12 +45,11 @@ const root = {
   userAddress: () => knex('patient_info.address_info_tbl').select('*'),
   postUserAddress: ({streetname, city, postal_code, province}) => {
     return knex('patient_info.address_info_tbl').insert({
-      addressid,
       streetname: streetname,
       city: city,
       postal_code: postal_code,
       province: province
-    })
+    }).returning('addressid')
   },
   postUserInfo: ({userid, addressid, first_name, last_name, phone_number, email, birthdate, 
     date_became_patient, sex}) => {

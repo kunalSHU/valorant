308919274f6949c8d2abb4abc8ea7fa9604b1e6f
ShabazBadshah <badshah.shabaz@gmail.com>
2020-08-21 18:55:05 -0400

    Moved authentication logic from SignIn view to users service

--- frontend/src/services/user/index.js
+++ frontend/src/services/user/index.js
@@ -5,24 +5,20 @@ import users from "../../data/users";
 
 // TODO rename folder from user to account
 
+const API_GATEWAY_ENDPOINT = "http://142.1.46.70:8082";
+
+const PROMISE_REQUEST_DELAY_MS = 1500;
+
+// TODO
 export const getAccountInfoByEmail = email => {
   return new Promise(resolve => {
     setTimeout(() => {
-      axios
-        .get(
-          `http://dh2010pc01.utm.utoronto.ca:8086/account/find?email=${email}`
-        )
-        .then(response => {
-          const { data } = response.data;
-          resolve(data["foundAccount"]);
-        })
-        .catch(err => {
-          return {};
-        });
+      resolve(email);
     }, 700);
   });
 };
 
+// TODO
 export const getAllUsers = () => {
   return new Promise(resolve => {
     setTimeout(() => {
@@ -30,5 +26,37 @@ export const getAllUsers = () => {
         users: users
       });
     }, 700);
+  });
+};
+
+export const authenticateUser = (emailAddress, password) => {
+  localStorage.setItem("isAuthenticated", "false");
+  localStorage.setItem("email", "");
+  localStorage.setItem("accountId", "");
+
+  return new Promise(resolve => {
+    setTimeout(() => {
+      axios
+        .post(`${API_GATEWAY_ENDPOINT}/account/auth`, {
+          emailAddress,
+          password
+        })
+        .then(response => {
+          localStorage.setItem("isAuthenticated", "true");
+          localStorage.setItem("accountEmail", emailAddress);
+          localStorage.setItem("authToken", response.data.jwt_token);
+          localStorage.setItem(
+            "accountId",
+            response.data.data.foundAccount.account_id
+          );
+          resolve({ isAuthenticated: true });
+        })
+        .catch(() => {
+          resolve({
+            isAuthenticated: false,
+            errMessage: "Incorrect login credentials"
+          });
+        });
+    }, PROMISE_REQUEST_DELAY_MS);
   });
 };

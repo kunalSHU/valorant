e6277584df3d0d8cc8c177e1d81d84f959484c24
ShabazBadshah <badshah.shabaz@gmail.com>
2020-08-22 00:54:15 -0400

    Updated default DB accounts with unique JWT tokens

--- api-gateway/docker/db/accounts_database_init.sql
+++ api-gateway/docker/db/accounts_database_init.sql
@@ -23,48 +23,48 @@ CREATE TABLE permissions_tbl(
 );
 
 
--- Doctor
+-- Doctor Account
 INSERT INTO accounts_tbl(email_address, password_hash, password_salt) VALUES(
   'doctor@email.com',
-  '$2b$08$f8v.elIjKkbBdeBvCKX66ej/svbBcexo28.gfplFH8K4hyKtFkM0C',
-  '$2b$08$pLI.wYnpOPD0hsYmAqlh..'
+  '$2b$08$nX/R4AK07HIyV7FjXkrLlel.wWDNE0TGaf5tzCGPEfU2La/.AdyjC',
+  '$2b$08$nX/R4AK07HIyV7FjXkrLle'
 ) RETURNING account_id;
 
 INSERT INTO tokens_tbl(account_id, jwt_token) VALUES (
   (SELECT account_id FROM accounts_tbl WHERE email_address='doctor@email.com'),
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXJyRGF0ZSI6MTU5NjAyMzY5MDYyMSwiaWF0IjoxNTk2MDIzNjkwLCJleHAiOjE1OTk2MjM2OTB9.GLU7TAMYszrpOy9arXIXTe8B8OcGPpKC0Shd-ydohqQ');
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXJyRGF0ZSI6MTU5ODA3MTk0MzIyOCwiaWF0IjoxNTk4MDcxOTQzLCJleHAiOjE2MDE2NzE5NDN9.yGpDLkkBJ8U5KKeYs5R4Wt0vh-CoX7_aNZsP1thAfO4');
 
 INSERT INTO permissions_tbl(account_id, account_role) VALUES (
   (SELECT account_id FROM accounts_tbl WHERE email_address='doctor@email.com'), 
   'doctor');
 
 
--- Patient User
+-- Patient Account
 INSERT INTO accounts_tbl(email_address, password_hash, password_salt) VALUES(
-  'badshah.shabaz@email.com',
-  '$2b$08$f8v.elIjKkbBdeBvCKX66ej/svbBcexo28.gfplFH8K4hyKtFkM0C',
-  '$2b$08$pLI.wYnpOPD0hsYmAqlh..'
+  'patient@email.com',
+  '$2b$08$b/epqc/kykUDVKwJ8NQ5cuW32kS72MXjqBgSLFIB5b9/LuRXW8Kja',
+  '$2b$08$b/epqc/kykUDVKwJ8NQ5cu'
 ) RETURNING account_id;
 
 INSERT INTO tokens_tbl(account_id, jwt_token) VALUES (
-  (SELECT account_id FROM accounts_tbl WHERE email_address='badshah.shabaz@email.com'),
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXJyRGF0ZSI6MTU5NjAyMzY5MDYyMSwiaWF0IjoxNTk2MDIzNjkwLCJleHAiOjE1OTk2MjM2OTB9.GLU7TAMYszrpOy9arXIXTe8B8OcGPpKC0Shd-ydohqQ');
+  (SELECT account_id FROM accounts_tbl WHERE email_address='patient@email.com'),
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXJyRGF0ZSI6MTU5ODA3MTQ1NDE1NSwiaWF0IjoxNTk4MDcxNDU0LCJleHAiOjE2MDE2NzE0NTR9.Ogn0AYCcOp5OWI3hVG7qTQTwVvkt8TK-ryXUP2nLWb0');
 
 INSERT INTO permissions_tbl(account_id, account_role) VALUES (
-  (SELECT account_id FROM accounts_tbl WHERE email_address='badshah.shabaz@email.com'), 
+  (SELECT account_id FROM accounts_tbl WHERE email_address='patient@email.com'), 
   'patient');
 
 
--- Receptionist
+-- Receptionist Account
 INSERT INTO accounts_tbl(email_address, password_hash, password_salt) VALUES(
   'receptionist@email.com',
-  '$2b$08$f8v.elIjKkbBdeBvCKX66ej/svbBcexo28.gfplFH8K4hyKtFkM0C',
-  '$2b$08$pLI.wYnpOPD0hsYmAqlh..'
+  '$2b$08$noGebGJQJGEuMQQKwwURfOSUc/StK9W0c7bCOeLtV.S9kE0HCSzfi',
+  '$2b$08$noGebGJQJGEuMQQKwwURfO'
 ) RETURNING account_id;
 
 INSERT INTO tokens_tbl(account_id, jwt_token) VALUES (
   (SELECT account_id FROM accounts_tbl WHERE email_address='receptionist@email.com'),
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXJyRGF0ZSI6MTU5NjAyMzY5MDYyMSwiaWF0IjoxNTk2MDIzNjkwLCJleHAiOjE1OTk2MjM2OTB9.GLU7TAMYszrpOy9arXIXTe8B8OcGPpKC0Shd-ydohqQ');
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXJyRGF0ZSI6MTU5ODA3MTcyNzM1MywiaWF0IjoxNTk4MDcxNzI3LCJleHAiOjE2MDE2NzE3Mjd9.9_RkSTwkmnUod6KRtj325bMljx3sYbn89ie2PalKG2Y');
 
 INSERT INTO permissions_tbl(account_id, account_role) VALUES (
   (SELECT account_id FROM accounts_tbl WHERE email_address='receptionist@email.com'), 

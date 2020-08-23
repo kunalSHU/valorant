979149f671b23cf0f30c3cb3f241fc48ad244cc4
root <root@dh2010pc01.utm.utoronto.ca>
2020-08-23 16:18:09 -0400

    Rabbitmq send and recieve done kind of

--- api-gateway/src/recieve.js
+++ api-gateway/src/recieve.js
@@ -2,7 +2,7 @@
 
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function (error0, connection) {
+amqp.connect('amqp://142.1.46.70:8089', function (error0, connection) {
     if (error0) {
         throw error0;
     }

6c076deae2509b73f7e721f24614d20a56cb0cac
kunalSHU <kunalshukla@hotmail.com>
2020-08-24 15:31:06 -0400

    Sends sounds

---
+++ services/bookings/src/send.js
@@ -0,0 +1,28 @@
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://142.1.46.70:8089', function (error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+
+    connection.createChannel(function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var queue = 'hello';
+        var msg = 'Hello World!';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        channel.sendToQueue(queue, Buffer.from(msg));
+
+        console.log(" [x] Sent %s", msg);
+    });
+    setTimeout(function () {
+        connection.close();
+        process.exit(0);
+    }, 500);
+});

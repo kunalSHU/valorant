cc3702252ac05a2de81e9d8ef3e3e8cd2729eaf8
kunalSHU <kunalshukla@hotmail.com>
2020-08-24 19:57:55 -0400

    Refactoring code

--- services/bookings/src/send.js
+++ services/bookings/src/send.js
@@ -3,100 +3,62 @@ var EventEmitter = require('events');
 var util = require('util');
 const amqp = require('amqplib/callback_api');
 
-function DbEventEmitter(){
-	EventEmitter.call(this);
+function DbEventEmitter() {
+    EventEmitter.call(this);
 }
 
 util.inherits(DbEventEmitter, EventEmitter);
 var dbEventEmitter = new DbEventEmitter;
 
 dbEventEmitter.on('update_status', (msg) => {
-	console.log('Status updated');
+    console.log('Status updated');
 });
 
-console.log("OK WE HERE?");
 const config = {
-	user: 'postgres',
-	database: 'bookings_db',
-	password: 'postgres',
-	port: 8083
+    user: 'postgres',
+    database: 'bookings_db',
+    password: 'postgres',
+    port: 8083
 };
 
 const pool = new pg.Pool(config);
 
-pool.connect(function(err, client, done) {
-	console.log("NOW HERE");
-	if(err){
-		console.log(err);
-	}
-	
-	client.on('notification', function(msg) {
-		let payload = JSON.parse(msg.payload);
-		console.log(payload);
-		console.log('PAY LOAD ENDED');
+pool.connect(function (err, client, done) {
+    console.log("NOW HERE");
+    if (err) {
+        console.log(err);
+    }
 
-		if(payload!= null){
-			amqp.connect('amqp://142.1.46.70:8089', function (error0, connection) {
-				if (error0) {
-					throw error0;
-				}
+    //Gets the payload if the trigger is executed
+    client.on('notification', function (msg) {
+        let payload = JSON.parse(msg.payload);
+        console.log(payload);
+        console.log('PAY LOAD ENDED');
 
-				connection.createChannel(function (error1, channel) {
-				if (error1) {
-							                throw error1;
-							            }
+        //If there is a payload, then send that payload to rabbitMQ
+        if (payload != null) {
+            amqp.connect('amqp://142.1.46.70:8089', function (error0, connection) {
+                if (error0) {
+                    throw error0;
+                }
 
-					            var queue = 'hello';
-					            var msg = `Appointment ID ${payload.appointmentid} status has been changed to ${payload.status_appt}`;
+                connection.createChannel(function (error1, channel) {
+                    if (error1) {
+                        throw error1;
+                    }
+                    var queue = 'hello';
+                    var msg = `Appointment ID ${payload.appointmentid} status has been changed to ${payload.status_appt}`;
 
-					            channel.assertQueue(queue, {
-							                durable: false
-							            });
+                    channel.assertQueue(queue, {
+                        durable: false
+                    });
 
-					            channel.sendToQueue(queue, Buffer.from(msg));
+                    channel.sendToQueue(queue, Buffer.from(msg));
+                    console.log(" [x] Sent %s", msg);
+                });
 
-					            console.log(" [x] Sent %s", msg);
-					        });
-				
-			});
-		}
-	});
-
-	client.query('LISTEN update_status');
+            });
+        }
+    });
+    client.query('LISTEN update_status');
 });
-
-//const pgClient = new pg.Client(connectionString);
-//console.log('CONNECTED WOHOO');
-//pgClient.connect();
-//console.log("OK NOW WHAT?");
-
-//const query = pg.Client.query('LISTEN');
-//console.log('AM I LISTENING?');
-// const amqp = require('amqplib/callback_api');
-
-// amqp.connect('amqp://142.1.46.70:8089', function (error0, connection) {
-//     if (error0) {
-//         throw error0;
-//     }
-
-//     connection.createChannel(function (error1, channel) {
-//         if (error1) {
-//             throw error1;
-//         }
-
-//         var queue = 'hello';
-//         var msg = 'Hello World!';
-
-//         channel.assertQueue(queue, {
-//             durable: false
-//         });
-
-//         channel.sendToQueue(queue, Buffer.from(msg));
-
-//         console.log(" [x] Sent %s", msg);
-//     });
-//     setTimeout(function () {
-//         connection.close();
-//         process.exit(0);
-//     }, 500);
-// });

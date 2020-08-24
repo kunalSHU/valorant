c0dcf98e4bf2395081e3c405a69f00d92dd7c606
root <root@dh2010pc01.utm.utoronto.ca>
2020-08-24 18:02:38 -0400

    Testing this shiz

--- services/bookings/db/notify.sql
+++ services/bookings/db/notify.sql
@@ -1,3 +1,3 @@
-CREATE TRIGGER updated_status AFTER UPDATE 
+CREATE TRIGGER notify_update_status AFTER UPDATE 
 ON bookings_info.appointments_info_basic_tbl
-FOR EACH ROW EXECUTE PROCEDURE notify_testevent();
+FOR EACH ROW EXECUTE PROCEDURE notify_update_status();
--- services/bookings/src/send.js
+++ services/bookings/src/send.js
@@ -1,15 +1,38 @@
-const config = {
-    user: 'postgres',
-    password: 'postgres',
-    host: '142.1.46.70:8083',
-    db: 'bookings_db',
-    table: 'bookings_info.appointments_info_basic_tbl'
+var pg = require('pg');
+var EventEmitter = require('events');
+var util = require('util');
+
+function DbEventEmitter(){
+	EventEmitter.call(this);
 }
 
-const connectionString = 'postgres://postgres:postgres@142.1.46.70:8083/bookings_db';
+util.inherits(DbEventEmitter, EventEmitter);
+var dbEventEmitter = new DbEventEmitter;
 
-const pgClient = new pg.Client(connectionString);
-pgClient.connect();
+dbEventEmitter.on('update_status', (msg) => {
+	console.log('Status updated');
+});
+
+
+pg.connect('postgres://postgres:postgres@142.1.46.70:8083/bookings_db', function(err, client){
+	if(err){
+		console.log(err);
+	}
+	
+	client.on('notification', function(msg) {
+		let payload = JSON.parse(msg.payload);
+	}):
+
+	client.query('LISTEN update_status');
+});
+
+//const pgClient = new pg.Client(connectionString);
+//console.log('CONNECTED WOHOO');
+//pgClient.connect();
+//console.log("OK NOW WHAT?");
+
+//const query = pg.Client.query('LISTEN');
+//console.log('AM I LISTENING?');
 // const amqp = require('amqplib/callback_api');
 
 // amqp.connect('amqp://142.1.46.70:8089', function (error0, connection) {
